343bdd42de29e761533132234e390c367b78be11
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-06-02 00:06:10 +0800

    tabs added, selected tab state done

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  state = {
+    selected: "stopwatch",
+    tabs: ["stopwatch", "pomodoro", "tabata", "custom"]
+  };
+
+  selectTab(index) {
+    this.setState({ selected: this.state.tabs[index] })
+    console.log(this.state.selected);
+  };
+
   render() {
     return (
       <div className="app">
-        TODO: App
+        <div className="tabs">
+          {this.state.tabs.map((tab, index) =>
+            <button className="tab btn btn-info" onClick={() => this.selectTab(index)} key={index}>{tab}</button>
+          )}
+        </div>
       </div>
     );
   };
