277fce69d9ce11cec10aef8634ef334a69c71626
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-06-07 10:34:35 +0800

    logic for tabata timer done

--- src/components/tabata.jsx
+++ src/components/tabata.jsx
@@ -3,6 +3,58 @@ import React, { Component } from "react";
 class Tabata extends Component {
   state = { time: 20, rest: 10, rounds: 8, started: false, break: false };
 
+  tabata = () => {
+    this.setState({ started: true });
+
+    // started: true
+    if (this.state.started) {
+      if (this.state.time > 0) {
+        // started and there is time
+        this.setState({ time: this.state.time - 1 })
+      } else {
+        // started and there is no time
+        this.setState({ break: true })
+      }
+    }
+
+    // break: true
+    if (this.state.break) {
+      if (this.state.rest > 0) {
+        // break and there is rest time
+        this.setState({ rest: this.state.rest - 1 })
+      } else {
+        // break === true and there is no time
+        if (this.state.rounds > 0) {
+          this.setState({ rounds: this.state.rounds - 1 })
+          this.setState({ break: false })
+          this.setState({ time: 10 })
+        } else {
+          console.log('finished')
+          this.setState({ started: false, break: false })
+        }
+      }
+    }
+  };
+
+  startTabata = () => {
+    setInterval(this.tabata, 1000) // 1-second intervals
+    if (this.state.rounds > 0 || this.state.time > 0 || this.state.rest > 0) {
+      this.setState({ rounds: this.state.rounds - 1 })
+      this.setState({ time: this.state.time - 1 })
+    }
+    this.setState({ started: true });
+  }
+
+  stopTabata = () => {
+    this.setState({ started: false });
+    clearInterval(this.tabata);
+  };
+
+  resetTabata = (event) => {
+    this.setState({ time: 20, rest: 10, rounds: 8, started: false, break: false });
+    clearInterval(this.tabata);
+  };
+
   handleTimeInput = (event) => { this.setState({ time: event.target.value }) };
   handleRestInput = (event) => { this.setState({ rest: event.target.value }) };
   handleRoundsInput = (event) => { this.setState({ rounds: event.target.value }) };
@@ -20,9 +72,9 @@ class Tabata extends Component {
           Rounds: <input type="text" pattern="[0-9]*" onChange={this.handleRoundsInput} value={this.state.rounds} />
         </h4>
         <div>
-          <button className="btn btn-danger">Start</button>
-          <button className="btn btn-danger">Stop</button>
-          <button className="btn btn-danger">Reset</button>
+          <button className="btn btn-danger" onClick={this.startTabata}>Start</button>
+          <button className="btn btn-danger" onClick={this.stopTabata}>Stop</button>
+          <button className="btn btn-danger" onClick={this.resetTabata}>Reset</button>
         </div>
       </div>
     );
