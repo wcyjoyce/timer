4f0e74fac30aacf02e5fb9f52824e8dd22c0b1e1
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-06-19 22:53:50 +0800

    input field values fixed once timer has started

--- src/components/tabata.jsx
+++ src/components/tabata.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 
+// TODO:
+// 1) tabata timer doesn't setInterval if inputs have been changed
+
 class Tabata extends Component {
   state = { time: 20, rest: 10, rounds: 8, started: false, break: false };
 
@@ -21,15 +24,16 @@ class Tabata extends Component {
         if (this.state.rounds > 0) {
           this.setState({ rounds: this.state.rounds - 1, break: false, time: 10 });
         } else {
-          console.log('finished');
           this.setState({ started: false, break: false });
+          console.log('finished');
         };
       };
     };
   };
 
   startTabata = () => {
-    this.tabata = setInterval(this.tabata, 1000) // 1-second intervals
+    this.tabata = setInterval(this.tabata, 1000); // 1-second intervals
+    // setInterval(this.tabata, 1000);
     if (this.state.rounds > 0 || this.state.time > 0 || this.state.rest > 0) {
       this.setState({ rounds: this.state.rounds - 1, time: this.state.time - 1 });
     };
@@ -41,7 +45,7 @@ class Tabata extends Component {
     clearInterval(this.tabata);
   };
 
-  resetTabata = (event) => {
+  resetTabata = () => {
     this.setState({ time: 20, rest: 10, rounds: 8, started: false, break: false });
     clearInterval(this.tabata);
   };
@@ -58,10 +62,14 @@ class Tabata extends Component {
       <div className="tabata">
         <h1>Tabata</h1>
         <h4>
-          Time: <input type="text" pattern="[0-9]*" onChange={this.handleTimeInput} value={this.state.time} />
-          Rest: <input type="text" pattern="[0-9]*" onChange={this.handleRestInput} value={this.state.rest} />
-          Rounds: <input type="text" pattern="[0-9]*" onChange={this.handleRoundsInput} value={this.state.rounds} />
+          Time: <input type="text" pattern="[0-9]*" onChange={this.handleTimeInput} />
+          Rest: <input type="text" pattern="[0-9]*" onChange={this.handleRestInput} />
+          Rounds: <input type="text" pattern="[0-9]*" onChange={this.handleRoundsInput} />
         </h4>
+        <div>
+          <h4>Round #{this.state.rounds}</h4>
+          <h4>{!this.state.break ? this.state.time : this.state.rest}</h4>
+        </div>
         <div>
           <button className="btn btn-danger" onClick={this.startTabata}>Start</button>
           <button className="btn btn-danger" onClick={this.stopTabata}>Stop</button>
